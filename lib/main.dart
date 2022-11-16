8f4a09c5e9238bc03ce7a3d9399f081856d3d9cd
athouba99 <athooba99@gmail.com>
2022-11-16 19:45:30 +0300

    task2

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,37 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-            body: Center(
-          child: Image.asset(
-            "assets/images/animal.jpg",
-            height: 300,
-            width: 300,
-          ),
+            // backgroundColor: pink ,
+            body: Column(
+          crossAxisAlignment: CrossAxisAlignment
+              .start, // for the text aligmnet, start is for left
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  right: 35,
+                  left:
+                      45), // .symmetric(horizontal: #) didn't work for this pic
+              child: Image.asset(
+                "assets/images/animal.jpg",
+                height: 300,
+                width: 300,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 10, left: 25), // adding space to the text box
+              child: Container(child: Text("Animal Name: Dolphin")),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Container(child: Text("Animal Age: old enough")),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Container(
+                  child: Text("Animal Gender: either male or female")),
+            ),
+          ],
         )));
   }
 }
